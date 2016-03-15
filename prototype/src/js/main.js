c9c3789cd229ab9fdc0d685c5191aefc37f68786
Joe Dalton <joe@sequence.com>
2016-03-15 01:19:26 -0700

    Added info boxes to slider

--- prototype/src/js/main.js
+++ prototype/src/js/main.js
@@ -9,4 +9,19 @@ $(function(){
 	$('#slider-1').slick(sliderSettings);
 
 	$('#slider-2').slick(sliderSettings);
+
+
+  // Slider info drop-downs
+  var $triggers = $('.slider .drop-trigger');
+
+  $triggers.click(function(){
+    var $this = $(this);
+    var $info = $this.siblings('.info-cont');
+    $info.addClass('show');
+
+    $info.click(function(){
+      $(this).removeClass('show');
+      $info.off('click');
+    });
+  });
 });
